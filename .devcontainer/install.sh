6c71ed603e1f02e1de9d42b1c8043ade840fd491
Brad <bradtaniguchi@gmail.com>
2021-08-15 02:58:48 +0000

    Update codespaces dev-container setup

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -15,22 +15,9 @@ nvm alias default node
 echo ">> updating git config"
 git config --global core.editor "nvim"
 
-# set vim as other editors in general
-echo ">> setting environment variables"
-export VISUAL=nvim
-export EDITOR="$VISUAL"
-echo "export VISUAL=nvim" >> ~/.bashrc
-echo "export EDITOR=nvim" >> ~/.bashrc
-
-# setup git completion
-echo ">> set git completion"
-echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
-
-
 # install neovim and cypress deps, required by dotfiles
 echo ">> installing dependencies"
 sudo apt install -y --no-install-recommends neovim
-
 
 # install npm packages
 echo ">> installing node deps"
@@ -38,10 +25,5 @@ npm ci
 
 # install global nx cli, not always required but useful to have
 npm i -g @nrwl/cli
-
-# manually install cypress, as it doesn't work out of the box for some reason
-# activate if needed
-# echo ">> re-installing cypress"
-# npx cypress install
 
 echo ">> done"
