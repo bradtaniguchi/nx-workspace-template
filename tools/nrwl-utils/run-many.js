e69f66e826f2ce7f29c7f46d02e3b6b8a93a020a
Brad <bradtaniguchi@gmail.com>
2021-12-21 00:15:24 +0000

    Refactored how projects are loaded

--- tools/nrwl-utils/run-many.js
+++ tools/nrwl-utils/run-many.js
@@ -6,14 +6,11 @@ const isMaster = branch === 'refs/heads/main';
 const baseSha = isMaster ? 'origin/main~1' : 'origin/main';
 
 (async () => {
-  const affected = await exec(
-    `npx nx print-affected --base=${baseSha} --target=${target}`
+  const affectedProjects = await exec(
+    `npx nx print-affected --base=${baseSha} --target=${target} --select=projects`
   ).then((res) => res.stdout.toString('utf-8'));
 
-  const array = JSON.parse(affected)
-    .tasks.map((t) => t.target.project)
-    .slice()
-    .sort();
+  const array = JSON.parse(affectedProjects).sort();
 
   const sliceSize = Math.max(Math.floor(array.length / jobCount), 1);
   const projects =
