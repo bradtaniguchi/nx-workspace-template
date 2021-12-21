30023617873031337000f88b7a5e486c898de912
Brad <bradtaniguchi@gmail.com>
2021-12-21 01:23:27 +0000

    Add action-factory function
    - will be used to create the other kinds.

---
+++ libs/common-ngrx/src/lib/util/http-action-type-params.ts
@@ -0,0 +1,21 @@
+import { ActionCreatorProps, NotAllowedCheck } from '@ngrx/store';
+import { NotAllowedInPropsCheck } from '@ngrx/store/src/models';
+
+/**
+ * "Wrapper type", used to define
+ * the types for the `htt-action-factory`.
+ *
+ * **note** the types are non-optimal, but for
+ * the user-facing scheme it should work.
+ */
+export interface HttpActionTypeParams<
+  RequestPayload extends unknown = unknown,
+  SuccessPaylod extends unknown = unknown,
+  FailurePayload extends unknown = unknown,
+  CancelPayload extends unknown = unknown
+> {
+  request: RequestPayload;
+  success: SuccessPaylod;
+  failure: FailurePayload;
+  cancel: CancelPayload;
+}
