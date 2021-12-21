11786e0e57e693498a1485991a7ac66c6ca3fea1
Brad <bradtaniguchi@gmail.com>
2021-12-20 16:12:18 -0800

    feat/add nrwl utils (#9)
    - Add run-many script
    - Update pipeline with run-many and caching

---
+++ tools/nrwl-utils/run-many.js
@@ -0,0 +1,32 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const [target, jobIndex, jobCount, branch] = process.argv.slice(2);
+const isMaster = branch === 'refs/heads/main';
+const baseSha = isMaster ? 'origin/main~1' : 'origin/main';
+
+(async () => {
+  const affected = await exec(
+    `npx nx print-affected --base=${baseSha} --target=${target}`
+  ).then((res) => res.stdout.toString('utf-8'));
+
+  const array = JSON.parse(affected)
+    .tasks.map((t) => t.target.project)
+    .slice()
+    .sort();
+
+  const sliceSize = Math.max(Math.floor(array.length / jobCount), 1);
+  const projects =
+    jobIndex < jobCount
+      ? array.slice(sliceSize * (jobIndex - 1), sliceSize * jobIndex)
+      : array.slice(sliceSize * (jobIndex - 1));
+
+  if (projects.length > 0) {
+    await exec(
+      `npx nx run-many --target=${target} --projects=${projects} --parallel`,
+      {
+        stdio: [0, 1, 2],
+      }
+    );
+  }
+})();
