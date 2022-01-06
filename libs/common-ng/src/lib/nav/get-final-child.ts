5ad761df149e8c38b20f4e617ce93dce05c50f8d
Brad <bradtaniguchi@gmail.com>
2022-01-05 17:11:31 -0800

    Add nav-utils (#32)
    Closes #29

---
+++ libs/common-ng/src/lib/nav/get-final-child.ts
@@ -0,0 +1,14 @@
+import { ActivatedRoute } from '@angular/router';
+
+/**
+ * Function that can be used to get the "final child" in the route.
+ * Can be mixed with the `ExtraOptions`'s `paramsInheritanceStrategy` "always"
+ * setting passed to the `RouterModule`.
+ *
+ * Common settings are available via `COMMON_NG_ROUTER_OPTIONS`
+ */
+export const getFinalChild = (root: ActivatedRoute) => {
+  let child = root;
+  while (child.firstChild) child = child.firstChild;
+  return child;
+};
