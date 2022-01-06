0b49704220d803e44031c05bcf9b9b323b4a51f4
Brad <bradtaniguchi@gmail.com>
2022-01-06 01:22:16 +0000

    Clean up generated files

--- apps/internal-ng/src/app/app.component.ts
+++ apps/internal-ng/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nx-workspace-template-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styles: [],
 })
-export class AppComponent {
-  title = 'internal-ng';
-}
+export class AppComponent {}
