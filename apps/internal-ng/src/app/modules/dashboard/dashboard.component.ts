80c4020d9fad92608997ec88159b4a4321368940
brad taniguchi <bradtaniguchi@gmail.com>
2022-01-11 21:41:51 -0800

    Add images for internal apps

--- apps/internal-ng/src/app/modules/dashboard/dashboard.component.ts
+++ apps/internal-ng/src/app/modules/dashboard/dashboard.component.ts
@@ -3,7 +3,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'nx-workspace-template-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: [],
+  styles: [
+    `
+      img {
+        max-width: 300px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {}
