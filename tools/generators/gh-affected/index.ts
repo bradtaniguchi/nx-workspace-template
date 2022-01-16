3bb8392992798c2c0e03fce0e809e386e775cd31
Brad <bradtaniguchi@gmail.com>
2022-01-16 15:48:58 -0800

    Add bare option to gh-affected

--- tools/generators/gh-affected/index.ts
+++ tools/generators/gh-affected/index.ts
@@ -19,9 +19,13 @@ export default async function (
      * By default this is 1, so each task is run in its own process.
      */
     batch?: number;
+    /**
+     * If we are to just return the `id`, rather than the entire task object.
+     */
+    bare?: boolean;
   }
 ) {
-  const { target, head, batch } = schema;
+  const { target, head, batch, bare } = schema;
   const isMain = head === 'refs/heads/main';
   const BASE_SHA = isMain ? 'origin/main~1' : 'origin/main';
   const output = await exec(
@@ -29,12 +33,18 @@ export default async function (
   );
   const affected = JSON.parse(output.stdout);
   const tasks = affected.tasks;
+  const toBare = (tasks: Array<unknown>) =>
+    tasks.map((task: { id?: string }) =>
+      task && typeof task.id === 'string' ? task.id : ''
+    );
   if (!batch) {
+    if (bare) return logger.log(JSON.stringify(toBare(tasks)));
     return logger.log(JSON.stringify(tasks));
   }
   const batchedTasks: unknown[] = [];
   const batchSize = Math.ceil(tasks.length / batch);
   for (let i = 0; i < tasks.length; i += batchSize)
     batchedTasks.push(tasks.slice(i, i + batchSize));
+  if (bare) return logger.log(JSON.stringify(toBare(batchedTasks)));
   logger.log(JSON.stringify(batchedTasks));
 }
