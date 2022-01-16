45baa81f27eda07be8732bae890a1d4e85ae86fb
Brad <bradtaniguchi@gmail.com>
2022-01-16 15:56:51 -0800

    Fix linting, and fix editor checks

--- tools/generators/gh-affected/index.ts
+++ tools/generators/gh-affected/index.ts
@@ -25,26 +25,30 @@ export default async function (
     bare?: boolean;
   }
 ) {
-  const { target, head, batch, bare } = schema;
-  const isMain = head === 'refs/heads/main';
-  const BASE_SHA = isMain ? 'origin/main~1' : 'origin/main';
-  const output = await exec(
-    `npx nx print-affected --base=${BASE_SHA} --target=${target}`
-  );
-  const affected = JSON.parse(output.stdout);
-  const tasks = affected.tasks;
-  const toBare = (tasks: Array<unknown>) =>
-    tasks.map((task: { id?: string }) =>
-      task && typeof task.id === 'string' ? task.id : ''
+  try {
+    const { target, head, batch, bare } = schema;
+    const isMain = head === 'refs/heads/main';
+    const BASE_SHA = isMain ? 'origin/main~1' : 'origin/main';
+    const output = await exec(
+      `npx nx print-affected --base=${BASE_SHA} --target=${target}`
     );
-  if (!batch) {
-    if (bare) return logger.log(JSON.stringify(toBare(tasks)));
-    return logger.log(JSON.stringify(tasks));
+    const affected = JSON.parse(output.stdout);
+    const tasks = affected.tasks;
+    const toBare = (tasks: Array<unknown>) =>
+      (tasks as Array<{ id?: string }>).map((task: { id?: string }) =>
+        task && typeof task.id === 'string' ? task.id : ''
+      );
+    if (!batch) {
+      if (bare) return logger.log(JSON.stringify(toBare(tasks)));
+      return logger.log(JSON.stringify(tasks));
+    }
+    const batchedTasks: unknown[] = [];
+    const batchSize = Math.ceil(tasks.length / batch);
+    for (let i = 0; i < tasks.length; i += batchSize)
+      batchedTasks.push(tasks.slice(i, i + batchSize));
+    if (bare) return logger.log(JSON.stringify(toBare(batchedTasks)));
+    logger.log(JSON.stringify(batchedTasks));
+  } catch (err) {
+    logger.error(err);
   }
-  const batchedTasks: unknown[] = [];
-  const batchSize = Math.ceil(tasks.length / batch);
-  for (let i = 0; i < tasks.length; i += batchSize)
-    batchedTasks.push(tasks.slice(i, i + batchSize));
-  if (bare) return logger.log(JSON.stringify(toBare(batchedTasks)));
-  logger.log(JSON.stringify(batchedTasks));
 }
